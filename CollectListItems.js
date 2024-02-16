7749db232b7d8df484b71ff410822af97ba5317a
GalaSport <146236308+GalaSport@users.noreply.github.com>
2024-02-16 16:24:21 +0200

    Add files via upload

---
+++ CollectListItems.js
@@ -0,0 +1,10 @@
+function extractText(){
+    let list = document.getElementById("items");
+    let result = [];
+    let items = Array.from(list.children);
+    for(let item of items){
+        result.push(item.textContent);
+    }
+    let textarea = document.getElementById("result");
+    textarea.value = result.join("\n");
+}
