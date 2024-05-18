869d6e1d0e8bd7bc7eb916fd6bb9044619364df3
GalaSport <146236308+GalaSport@users.noreply.github.com>
2024-05-18 23:01:04 +0300

    Add files via upload

---
+++ lastKsequenceNumber.js
@@ -0,0 +1,11 @@
+function lastKsequenceNumber(n, k){
+    let resultArray = [1,];
+    let sum = 1;    
+    for (let i = 1; i < n; i++) {
+        sum += ;
+        
+    }
+
+    console.log(resultArray);
+}
+lastKsequenceNumber(6, 3); //[1, 1, 2, 4, 7, 13]
