63ae70b1f866f8fcbdeb6204d4f9f179bedd07a0
GalaSport <146236308+GalaSport@users.noreply.github.com>
2024-05-18 23:20:01 +0300

    Update lastKsequenceNumber.js

--- lastKsequenceNumber.js
+++ lastKsequenceNumber.js
@@ -1,11 +1,13 @@
 function lastKsequenceNumber(n, k){
-    let resultArray = [1,];
-    let sum = 1;    
+    let newArr = [1];
+       
     for (let i = 1; i < n; i++) {
-        sum += ;
-        
+         let sum = 0;
+        for(let j = Math.max(0, i-k); j < i; J++){
+        sum += newArr[j];
+        }
+        newArr.push(sum);
     }
-
-    console.log(resultArray);
+    console.log(newArr.join(" ");
 }
 lastKsequenceNumber(6, 3); //[1, 1, 2, 4, 7, 13]
