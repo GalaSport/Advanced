1b0a0ddba55cf8bf64a25ed93d87716126f053c3
GalaSport <146236308+GalaSport@users.noreply.github.com>
2024-08-07 19:16:36 +0300

    Add files via upload

---
+++ Tickets2.js
@@ -0,0 +1,36 @@
+function tickets(array, sortingCriterion) {
+    let ticketsArray = [];
+    class Ticket {
+
+        constructor(destination, price, status) {
+
+            this.destination = destination;
+            this.price = price;
+            this.status = status;
+        }
+        static sort(array, criteria) {
+            return array.sort((a, b) => {
+                return criteria === "price" ?
+                    a[criteria] - b[criteria] :
+                    a[criteria].localeCompare(b[criteria])
+            }
+            )
+        }
+    }
+    for (let i = 0; i < array.length; i++) {
+
+        let tokens = array[i].split("|");
+        let destination = tokens[0];
+        let price = Number(tokens[1]);
+        let status = tokens[2];
+
+        let myTicket = new Ticket(destination, price, status);
+        ticketsArray.push(myTicket);
+    }
+    return Ticket.sort(ticketsArray, sortingCriterion)
+}
+let result = tickets(['Philadelphia|94.20|available',
+    'New York City|95.99|available',
+    'New York City|95.99|sold',
+    'Boston|126.20|departed'], "status");
+console.log(result);
